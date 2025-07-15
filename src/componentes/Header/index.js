7117c40aa96a9be4fe543695f244fc16eedc79c1
André <andreeeepicanco@gmail.com>
2025-07-14 22:03:21 -0400

    Aula 02 completa

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,18 @@
+import './estilo.css'
+import IconesHeader from '../IconesHeader';
+import OpcoesHeader from '../OpcoesHeader';
+import Logo from '../Logo'
+
+function Header () {
+
+    return (
+    <header className="App-header">''
+          <Logo/>
+          <OpcoesHeader/>
+          <IconesHeader/>
+      </header>
+    )
+
+}
+
+export default Header
