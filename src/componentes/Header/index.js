b6db16f4eb53aa174a9637a0df6f2400c480911a
André <andreeeepicanco@gmail.com>
2025-08-18 23:28:06 -0400

    Init Curso 03 - Fullstack

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -2,6 +2,7 @@ import IconesHeader from '../IconesHeader';
 import OpcoesHeader from '../OpcoesHeader';
 import Logo from '../Logo'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 const HeaderContainer = styled.header`
     background-color: #FFFF;
@@ -13,7 +14,9 @@ function Header () {
 
     return (
         <HeaderContainer>
-            <Logo/>
+            <Link to='/'>
+                <Logo/>
+            </Link>
             <OpcoesHeader/>
             <IconesHeader/>
         </HeaderContainer>
